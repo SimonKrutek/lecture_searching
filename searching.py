4a7425c5f3a9a6a23e869a40e063286b5515745a
SimonKrutek <krutek.simon@gmail.com>
2025-04-07 10:24:40 +0200

    4

--- searching.py
+++ searching.py
@@ -53,9 +53,45 @@ def pattern_search(seq, vzor):
     :param vzor: hledany vzor
     :return: Funkce vrátí množinu, ve které budou uloženy pozice (indexy) výskytu vzoru v
 sekvenci.
+    """
+    vzor_size = len(vzor)
+    ind = set()
 
+    left_idx = 0
+    right_idx = vzor_size
+    while right_idx < len(seq):
+        for idx in range(vzor_size):
+            if vzor[idx] != seq[left_idx + idx]:
+                break
+        else:
+            ind.add(left_idx + vzor_size // 2)
+
+        left_idx +=1
+        right_idx +=1
+    return ind
+
+
+def binary_search(seq, num):
     """
 
+    :param seq: prohledávaný seznam čísel
+    :param num: a hledané číslo
+    :return: Funkce vrátí index, na kterém se hledané číslo v sekvenci nachází. Pokud není číslo
+nalezeno, funkce vrátí hodnotu None.
+    """
+    low = 0
+    high = len(seq) - 1
+
+    while low <= high:
+        mid = (low + high)//2
+        if seq[mid] == num:
+            return mid
+        elif seq[mid] < num:
+            low = mid
+        else:
+            high = mid
+
+    return None
 
 def main():
     file_name = "sequential.json"
@@ -64,8 +100,21 @@ def main():
     seq = read_data(file_name, field="unordered_numbers")
     print(seq)
 
+
     cis = linear_search(seq, 102)
     print(cis)
+
+
+    seq = read_data(file_name, field="dna_sequence")
+    print(seq)
+    pozice = pattern_search(seq, "ATA")
+    print(pozice)
+
+    seq = read_data(file_name, field="ordered_numbers")
+    print(seq)
+    ind = binary_search(seq, 63)
+    print(ind)
+
 
 if __name__ == '__main__':
     main()
