fd9f4b7dd75a6d760bda079e557aa7277b4fa7de
SimonKrutek <krutek.simon@gmail.com>
2025-04-07 09:25:12 +0200

    2

--- searching.py
+++ searching.py
@@ -22,6 +22,41 @@ def read_data(file_name, field):
 
     return seq[field]
 
+def linear_search(seq, number):
+    """
+
+    :param seq: prohledávana sekvenci
+    :param number: hledane cislo
+    :return: Funkce vrátí slovník se dvěma klíči. Pod prvním klíčem positions bude uložen
+seznam pozic (indexů). Pod druhým klíčem count bude uložen počet výskytů
+hledaného čísla.
+    """
+    ind=[]
+    count = 0
+
+    idx = 0
+    while idx < len(seq):
+        if seq[idx] == number:
+            ind.append(idx)
+            count += 1
+        idx += 1
+
+    return {
+        "position": ind,
+        "count": count,
+    }
+
+def pattern_search(seq, vzor):
+    """
+
+    :param seq: prohledávanou sekvenci
+    :param vzor: hledany vzor
+    :return: Funkce vrátí množinu, ve které budou uloženy pozice (indexy) výskytu vzoru v
+sekvenci.
+
+    """
+
+
 def main():
     file_name = "sequential.json"
 
@@ -29,7 +64,8 @@ def main():
     seq = read_data(file_name, field="unordered_numbers")
     print(seq)
 
-
+    cis = linear_search(seq, 102)
+    print(cis)
 
 if __name__ == '__main__':
     main()
